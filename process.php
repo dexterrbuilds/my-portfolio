0279c32d70668bca53ef566b3507369ff4531c9e
thetechrat <93471443+thetechrat@users.noreply.github.com>
2022-05-30 08:49:07 -0700

    Update process.php

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "you@yourdomain.com";
+    $email_to = "alameenthetechrat@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
